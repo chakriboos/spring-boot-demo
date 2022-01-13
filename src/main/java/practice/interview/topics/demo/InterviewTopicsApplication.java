f3c5dceb0f74acf81673f0bb13d986bf2bd0f30c
Chakradhara Reddy Dwarasala <chakri.boost@gmail.com>
2022-01-13 16:44:29 -0500

    Spring boot initial structure committed

---
+++ src/main/java/practice/interview/topics/demo/InterviewTopicsApplication.java
@@ -0,0 +1,13 @@
+package practice.interview.topics.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewTopicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/interview/topics/demo/InterviewTopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.interview.topics.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTopicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
